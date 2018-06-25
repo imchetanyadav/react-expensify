9956f8062e8ca9c86d5796840995a4b59a6c6ddb
Chetan Yadav <imchetanyadav@gmail.com>
2018-06-25 17:55:07 +0530

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+
+module.exports = (env) => {
+  const isProduction = env === 'production';
+  return {
+    entry: './src/app.jsx',
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js',
+      publicPath: '/',
+    },
+    resolve: {
+      modules: [__dirname, 'node_modules'],
+      alias: {},
+      extensions: ['.js', '.jsx'],
+    },
+    module: {
+      rules: [
+        {
+          loader: 'babel-loader',
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+        }, {
+          test: /\.s?css$/,
+          use: [
+            'style-loader',
+            'css-loader',
+            'sass-loader',
+          ],
+        },
+      ],
+    },
+    devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true,
+    },
+  };
+};
